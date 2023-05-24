5362eafe0768fdd5dd440eca9b4650385898bdd3
Afwan <tmariefafwan@gmail.com>
2023-05-24 19:57:59 +0700

    landing page

---
+++ resources/views/partials/side.blade.php
@@ -0,0 +1,7 @@
+<nav id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+        <li class="sidebar-brand"><a href="#page-top">Start Bootstrap</a></li>
+        <li class="sidebar-nav-item"><a href="#about">About</a></li>
+        <li class="sidebar-nav-item"><a href="{{ route('login') }}">Login</a></li>
+    </ul>
+</nav>
